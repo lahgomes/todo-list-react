2085ddddf4dc0899e5f4e401e4f0a650e8760a54
Larissa <larissa_gomes19@hotmail.com>
2021-12-02 08:50:35 -0300

    function remove button

--- src/App.js
+++ src/App.js
@@ -15,9 +15,20 @@ function App() {
   
   const handleTodoSubmit = (e) => {
     e.preventDefault();
+    if (item === '') {
+      return      
+    }
 
     const newItems = [...items]
     newItems.push(item)
+
+    setItems(newItems)
+    setItem('')
+  }
+
+  const handleDelete = (index) => {
+    const newItems = [...items]
+    newItems.splice(index, 1)
 
     setItems(newItems)
   }
@@ -26,7 +37,7 @@ function App() {
     <>      
       {
         !showTodo && 
-        <div className="presentation" data-presentation>
+        <div className="presentation">
           <h1 className="presentation__welcome">
             Welcome to your Todo List!
           </h1>
@@ -39,25 +50,27 @@ function App() {
       }
       {
         showTodo && 
-        <div className="list hidden" data-todolist>
+        <div className="list hidden">
           <h1 className="list__title">
             {name}'s Todo List
           </h1>
                    
           <form className="list__form" onSubmit={handleTodoSubmit}>
-            <input className="list__input" type="text" placeholder="Add your new task here" value={item} onChange={(e) => setItem(e.target.value)} data-input/>
-            <button className="list__button" data-add>Add
+            <input className="list__input" type="text" placeholder="Add your new task here" value={item} onChange={(e) => setItem(e.target.value)}/>
+            <button className="list__button">Add
               <i className="fas fa-plus"></i>
             </button>
           </form>
 
-          <ul className="list__items" data-list>
+          <ul className="list__items">
             {
               items.map((item, index) => (
                 <li key={index}>
-                  <input type="checkbox" />
+                  <input type="checkbox" id={`item-${index}`}/>
+                  <label htmlFor={`item-${index}`}>
                   {item}
-                  <button>Delete</button>
+                  </label>
+                  <button onClick={() => handleDelete(index)}>Delete</button>
                 </li>
               ))
             }
